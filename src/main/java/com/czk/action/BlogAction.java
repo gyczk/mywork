4351685b791dd778ecfdc5bb556c6726b9fe69d8
czk <gyczk888@163.com>
2018-11-28 19:17:30 +0800

    富文本页面

--- src/main/java/com/czk/action/BlogAction.java
+++ src/main/java/com/czk/action/BlogAction.java
@@ -2,12 +2,19 @@ package com.czk.action;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.thymeleaf.context.WebContext;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 @RequestMapping("/sys/blog")
 public class BlogAction {
 
-    public String addPage(){
+    public String addPage(HttpServletRequest request, HttpServletResponse response, ServletContext servletContext){
+        WebContext wc = new WebContext(request, response, servletContext, request.getLocale());
+
         return "";
     }
 
